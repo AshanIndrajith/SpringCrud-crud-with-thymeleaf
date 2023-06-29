ad238b9a731c839c0b7e777fe687d3cf46c6b0bc
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2023-06-29 10:37:14 +0530

    update

--- SpringCrud/SpringCrud/src/main/java/com/example/SpringCrud/Entity/Entity.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringCrud.Entity;
-
-
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-@EntityScan
-public class Entity {
-}
--- SpringCrud/SpringCrud/src/main/java/com/example/SpringCrud/Service/Service.java
+++
@@ -1,4 +0,0 @@
-package com.example.SpringCrud.Service;
-
-public class Service {
-}
